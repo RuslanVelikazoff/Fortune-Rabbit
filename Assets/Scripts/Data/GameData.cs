982d672f11c4febad94334f0870bb5a9d4d9d0b3
Ruslan Velikazov <123361452+RuslanVelikazoff@users.noreply.github.com>
2024-10-03 08:58:03 +0300

    Create audioManager and button prefab

--- Assets/Scripts/Data/GameData.cs
+++ Assets/Scripts/Data/GameData.cs
@@ -1,9 +1,28 @@
 public class GameData
 {
-    
+    public bool[] levelUnlock = new bool[9];
+    public bool[] levelCompleted = new bool[9];
     
     public GameData()
     {
-        
+        levelUnlock[0] = true;
+        levelUnlock[1] = false;
+        levelUnlock[2] = false;
+        levelUnlock[3] = false;
+        levelUnlock[4] = false;
+        levelUnlock[5] = false;
+        levelUnlock[6] = false;
+        levelUnlock[7] = false;
+        levelUnlock[8] = false;
+
+        levelCompleted[0] = false;
+        levelCompleted[1] = false;
+        levelCompleted[2] = false;
+        levelCompleted[3] = false;
+        levelCompleted[4] = false;
+        levelCompleted[5] = false;
+        levelCompleted[6] = false;
+        levelCompleted[7] = false;
+        levelCompleted[8] = false;
     }
 }
